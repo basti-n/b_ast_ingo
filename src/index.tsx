9f9e5a24042b92ffed91485c780a741eefbe870f
Sebastian <sebastian.neumair@gmail.com>
2021-01-18 22:42:35 +0100

    Fix Font Loading in Safari

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ export const GlobalStyle = createGlobalStyle`
   }
   body {
     height: 100vh; 
-    font-family: 'Balsamiq Sans', cursive; 
+    font-family: 'Balsamiq Sans', 'Roboto', cursive; 
     font-weight: lighter; 
     background-color: #8BC6EC;
     background-image: linear-gradient(135deg, #8BC6EC 0%, #9599E2 100%);
